5102edada5e5f409e2e32c1d75f85ddd641ea4d6
Ray <34339116+r4yfx@users.noreply.github.com>
2018-01-19 19:40:39 +0000

    v0.3
    *Amendments made to the influx output
    *nulled the connection string to stop errors coming through.

--- wsus-stats2.ps1
+++ wsus-stats2.ps1
@@ -9,7 +9,7 @@
         NAME:  wsus-stats.ps1
         ORIGINAL NAME: wsus.ps1
         LASTEDIT: 19/01/2018
-        VERSION: 0.2
+        VERSION: 0.3
         KEYWORDS: WSUS, Grafana 
    
         .Link
@@ -22,7 +22,7 @@
  #>
 
 Import-Module PoshWSUS
-Connect-PSWSUSServer
+Connect-PSWSUSServer | Out-Null
 
 #region: Data Collecting
 # Collect Client Information
@@ -59,45 +59,65 @@ $wsusneed = @($updatesummary | ?{$_.Needed -ge 1})
 $wsusfailed = @($updatesummary | ?{$_.Failed -ge 1})
 $wsusreboot = @($updatesummary | ?{$_.PendingReboot -ge 1})
 
+#region: Prepare all Groups
+$wsusvtec = Get-WsusComputer -ComputerTargetGroups 4_VTEC
+$wsusmgmt = Get-WsusComputer -ComputerTargetGroups 0_Management
+$wsusutila = Get-WsusComputer -ComputerTargetGroups 1_Utilita
+$wsusoceana = Get-WsusComputer -ComputerTargetGroups 97_Oceana
+$wsusnewdaya = Get-WsusComputer -ComputerTargetGroups 8_Newday-Test
+$wsushome = Get-WsusComputer -ComputerTargetGroups 12_HomeBase
+$wsusinsightt = Get-WsusComputer -ComputerTargetGroups 0_Insight-Test
+$wsusutilb = Get-WsusComputer -ComputerTargetGroups 11_Utilita2
+$wsusfc = Get-WsusComputer -ComputerTargetGroups 3_FlightCentre
+$wsusinsightp = Get-WsusComputer -ComputerTargetGroups 0_Insight-Prod
+$wsusnewdayd = Get-WsusComputer -ComputerTargetGroups 9_Newday-Dev
+$wsusjourney = Get-WsusComputer -ComputerTargetGroups 14_JourneyCall
+$wsusswift = Get-WsusComputer -ComputerTargetGroups 13_Swift
+$wsusasra = Get-WsusComputer -ComputerTargetGroups 6_ASRA
+$wsusdemo = Get-WsusComputer -ComputerTargetGroups 99_Demo
+$wsusnewdayp = Get-WsusComputer -ComputerTargetGroups 7_Newday-Prod
+$wsusdx = Get-WsusComputer -ComputerTargetGroups 2_DX
+$wsusverint = Get-WsusComputer -ComputerTargetGroups 0_Verint-Bundle3
+
+#endregion
 
 #region: # InfluxDB Output for Telegraf
  
 $Count = $clienthostswina.Count
-$body="Windows Server 2012 R2=$Count"
+$body="wsus-stats windows2012r2=$Count"
 Write-Host $body 
 $Count = $clienthostswinb.Count
-$body="Windows Server 2008 R2 Datacenter Edition=$Count"
+$body="wsus-stats windows2008r2dced=$Count"
 Write-Host $body 
 $Count = $clienthostswinc.Count
-$body="Windows Server 2008 R2 Standard Edition=$Count"
+$body="wsus-stats windows2008stded=$Count"
 Write-Host $body
 $Count = $clienthostswind.Count
-$body="Windows Server 2012=$Count"
+$body="wsus-stats windows2012=$Count"
 Write-Host $body
 $Count = $clienthostswine.Count
-$body="Windows 7=$Count"
+$body="wsus-stats windows7=$Count"
 Write-Host $body
 $Count = $wsusupdatea.Count
-$body="Critical Updates=$Count"
+$body="wsus-stats criticalupdates=$Count"
 Write-Host $body
 $Count = $wsusupdateb.Count
-$body="Updates=$Count"
+$body="wsus-stats updates=$Count"
 Write-Host $body
 $Count = $wsusupdatec.Count
-$body="Service Packs=$Count"
+$body="wsus-stats sp=$Count"
 Write-Host $body
 $Count = $client.Count
-Write-Host "`"Number of Clients`"": "$Count,"
-$body="Number of Clients=$Count"
+$body="wsus-stats numberofclients=$Count"
 Write-Host $body
 $Count = $wsusneed.Count
-$body="Req-Updates=$Count"
+$body="wsus-stats requpdates=$Count"
 Write-Host $body
 $Count = $wsusfailed.Count
-$body="Failed=$Count"
+$body="wsus-stats failed=$Count"
 Write-Host $body
 $Count = $wsusreboot.Count
-$body="Req-Reboot=$Count"
+$body="wsus-stats reqreboot=$Count"
 Write-Host $body
 
 
